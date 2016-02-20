0d32ea62a85bec223c7ce1d0ddae06792de88b70
exequiel <exequiel222@gmail.com>
2016-02-20 04:06:58 -0300

    Integración con fabric

--- app/src/main/java/com/exequiel/shopcenter/app/ShopCenterApplication.java
+++ app/src/main/java/com/exequiel/shopcenter/app/ShopCenterApplication.java
@@ -1,6 +1,8 @@
 package com.exequiel.shopcenter.app;
 
 import android.support.multidex.MultiDexApplication;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by exequiel on 09/02/2016.
@@ -10,6 +12,7 @@ public class ShopCenterApplication extends MultiDexApplication{
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
     }
 
--- app/src/main/java/com/exequiel/shopcenter/app/presentacion/activitys/SplashAwesome.java
+++ app/src/main/java/com/exequiel/shopcenter/app/presentacion/activitys/SplashAwesome.java
@@ -24,7 +24,7 @@ public class SplashAwesome extends AwesomeSplash {
 
         //Customize Circular Reveal
         configSplash.setBackgroundColor(R.color.primary); //any color you want form colors.xml
-        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setAnimCircularRevealDuration(2500); //int ms
         configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
         configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
 
